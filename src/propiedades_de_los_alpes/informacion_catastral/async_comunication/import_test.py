a7628bf73c25f9ddd26772d25a5d94767a735783
Juan Camilo Ramirez <jc.ramirezg12@uniandes.edu.co>
2024-02-24 22:50:12 +0000

    comunicacion asincrona

--- src/propiedades_de_los_alpes/informacion_catastral/async_comunication/import_test.py
+++
@@ -1,3 +0,0 @@
-from propiedades_de_los_alpes.informacion_catastral.modulos.aplicacion.servicios import ServicioCatastro
-
-print("Import test")
--- src/propiedades_de_los_alpes/seekwork/dominio/fabricas.py
+++
@@ -1,7 +0,0 @@
-
-from abc import ABC, abstractmethod
-
-class Fabrica(ABC):
-    @abstractmethod
-    def crear_objeto(self, obj: any) -> any:
-        ...
