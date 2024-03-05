9c6cb11db0cfd35f6880f29fafb15c33c28ac7d6
Crhistian Mauricio Suárez Garnica <c.suarezg@uniandes.edu.co>
2024-03-04 19:50:45 -0500

    Adjust

--- src/propiedades/modulos/infraestructura/mapeadores.py
+++ src/propiedades/modulos/infraestructura/mapeadores.py
@@ -1,33 +1,25 @@
 from propiedades.seedwork.dominio.repositorios import Mapeador
-from propiedades.modulos.dominio.objetos_valor import NombreAero, Odo, Leg, Segmento, Itinerario, CodigoIATA
-from propiedades.modulos.dominio.entidades import Aeropuerto, Propiedad
+from propiedades.modulos.dominio.entidades import Propiedad
 from .dto import Propiedad as PropiedadDTO
 
-class MapeadorPropiedad(Mapeador):
-    _FORMATO_FECHA = '%Y-%m-%dT%H:%M:%SZ'
-
-    
+class MapeadorPropiedad(Mapeador):    
     def obtener_tipo(self) -> type:
         return Propiedad.__class__
 
     def entidad_a_dto(self, entidad: Propiedad) -> PropiedadDTO:
         
         propiedad_dto = PropiedadDTO()
-        propiedad_dto.fecha_creacion = entidad.fecha_creacion
-        propiedad_dto.fecha_actualizacion = entidad.fecha_actualizacion
-        propiedad_dto.id = str(entidad.id)
-
-        itinerarios_dto = list()
-        
-        for itinerario in entidad.itinerarios:
-            itinerarios_dto.extend(self._procesar_itinerario(itinerario))
-
-        propiedad_dto.itinerarios = itinerarios_dto
+        propiedad_dto.caracteristica = str(entidad.caracteristica)
+        propiedad_dto.ciudad = str(entidad.ciudad)
+        propiedad_dto.id_propiedad = int(entidad.id_propiedad)
+        propiedad_dto.precio_max = int(entidad.precio_max)
+        propiedad_dto.precio_min = int(entidad.precio_min)
+        propiedad_dto.tamano_max = int(entidad.tamano_max)
+        propiedad_dto.tamano_min = int(entidad.tamano_min)
+        propiedad_dto.tipo = str(entidad.tipo)
 
         return propiedad_dto
 
     def dto_a_entidad(self, dto: PropiedadDTO) -> Propiedad:
         propiedad = Propiedad(dto.caracteristica, dto.ciudad, dto.id_propiedad, dto.precio_max, dto.precio_min, dto.tamano_max, dto.tamano_min, dto.tipo)
-  
-        
         return propiedad
